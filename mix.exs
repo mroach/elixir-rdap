eb3356cee1fd2cbf8228310c6b6e501c1ac5ad8c
mroach <mroach@mroach.com>
2018-09-18 21:17:56 +0200

    Remove generated

--- mix.exs
+++ mix.exs
@@ -11,7 +11,6 @@ defmodule RDAP.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,7 +18,6 @@ defmodule RDAP.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:poison, "~> 3.1"},
@@ -27,8 +25,6 @@ defmodule RDAP.MixProject do
       {:inet_cidr, "~> 1.0.0"},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
